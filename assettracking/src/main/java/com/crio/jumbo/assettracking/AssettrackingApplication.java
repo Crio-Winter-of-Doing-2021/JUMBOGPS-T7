5bda47fb5e7bbd0f7eb8c23ae652d6f0f65c5515
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2021-03-09 16:35:58 +0000

    created spring boot project

---
+++ assettracking/src/main/java/com/crio/jumbo/assettracking/AssettrackingApplication.java
@@ -0,0 +1,13 @@
+package com.crio.jumbo.assettracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssettrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssettrackingApplication.class, args);
+	}
+
+}
